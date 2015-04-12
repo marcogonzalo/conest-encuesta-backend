9cffbe1200c2f02cad43cede051876bf444c96ff
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2015-04-11 22:32:04 -0430

    CORS

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -4,6 +4,14 @@ module Api
 		  # Prevent CSRF attacks by raising an exception.
 		  # For APIs, you may want to use :null_session instead.
 		  # protect_from_forgery with: :exception
+
+			before_filter :set_access_control_headers
+
+			def set_access_control_headers
+				headers['Access-Control-Allow-Origin'] = 'http://localhost:9000'
+				headers['Access-Control-Request-Method'] = 'GET, OPTIONS, HEAD, POST, PUT'
+				headers['Access-Control-Allow-Headers'] = 'x-requested-with,Content-Type, Authorization'
+			end
 		end
 	end
 end
