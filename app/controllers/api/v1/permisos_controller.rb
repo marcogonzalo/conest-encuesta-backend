2e69b7eecdcd9bef52de0f45f376028ee238049d
Marco Gómez <marcogonzalo@gmail.com>
2015-08-03 00:36:19 -0430

    quitado filtro cancancan en servicio para verificar si un usuario tiene un permiso

--- app/controllers/api/v1/permisos_controller.rb
+++ app/controllers/api/v1/permisos_controller.rb
@@ -4,8 +4,10 @@ module Api
 #			load_and_authorize_resource
 			before_action :cargar_permisos # call this after load_and_authorize else it gives a cancan error
 
-	 		def puede]
-				puede = @permisos_de_usuario.include?(params[:nombre_permiso])
+	 		def puede
+	 			puts params[:nombre_permiso]
+				puede = @current_user.super_admin? ? true : @permisos_de_usuario.include?(params[:nombre_permiso])
+				puts puede
 				render json: { puede: puede }
 			end
 		end
