eddb40223b79a912aa0d951ec3c2eac1f58d4879
Marco Gómez <marcogonzalo@gmail.com>
2015-11-08 21:36:29 -0430

    pase al modelo de acciones de sincronizacion de asignaturas y estudiantes en periodo

--- app/controllers/api/v1/periodos_academicos_controller.rb
+++ app/controllers/api/v1/periodos_academicos_controller.rb
@@ -33,7 +33,17 @@ module Api
 
               @periodo_academico = PeriodoAcademico.find_or_initialize_by(periodo: datos_conest['periodo_academico_id'])
 
-              sincronizar_periodo(@periodo_academico, respuesta_conest, datos_conest, instrumento_id)
+              estatus = @periodo_academico.sincronizar_periodo(respuesta_conest, datos_conest, instrumento_id)
+              case estatus
+              when :created
+                render json: { estatus: respuesta_conest['estatus'], mensaje: "Período #{@periodo_academico.periodo} registrado" }, status: :created
+              when :not_modified
+                render json: { estatus: respuesta_conest['estatus'], mensaje: "Período #{@periodo_academico.periodo} sin modificaciones" }, status: :not_modified
+              when :ok
+                render json: { estatus: respuesta_conest['estatus'], mensaje: "Período #{@periodo_academico.periodo} actualizado", sincronizacion: @periodo_academico.sincronizacion }, status: :ok
+              else
+                nil
+              end
             else
               render json: { estatus: respuesta_conest['estatus'], mensaje: respuesta_conest['mensaje'] }, status: :bad_request
             end
@@ -88,7 +98,15 @@ module Api
 
               instrumento_id = Instrumento.last;
 
-              sincronizar_periodo(@periodo_academico, respuesta_conest, datos_conest, instrumento_id)              
+              estatus = @periodo_academico.sincronizar_periodo(respuesta_conest, datos_conest, instrumento_id)              
+              case estatus
+              when :not_modified
+                render json: { estatus: respuesta_conest['estatus'], mensaje: "Período #{@periodo_academico.periodo} sin modificaciones" }, status: :not_modified
+              when :ok
+                render json: { estatus: respuesta_conest['estatus'], mensaje: "Controles de consultas para período #{@periodo_academico.periodo} actualizados", sincronizacion: @periodo_academico.sincronizacion }, status: :ok
+              else
+                nil
+              end
             else
               render json: { estatus: respuesta_conest['estatus'], mensaje: respuesta_conest['mensaje'] }, status: :bad_request
             end
@@ -114,37 +132,14 @@ module Api
 
           if response.code.eql?(200)
             if respuesta_conest['estatus'] == 'OK'
-              # Datos de la respuesta enviada por Conest
-              datos_conest = respuesta_conest['datos']
-
-              # Si el hash es el mismo no hay cambios en el listado
-              if @periodo_academico.estudiantes_hash_sum.eql?(respuesta_conest['sha1_sum'])
+              estatus = @periodo_academico.sincronizar_estudiantes(respuesta_conest)
+              case estatus
+              when :not_modified
                 render json: { estatus: respuesta_conest['estatus'], mensaje: "Período #{@periodo_academico.periodo} sin modificaciones" }, status: :not_modified
-              else
-                @periodo_academico.update(estudiantes_hash_sum: respuesta_conest['sha1_sum'], sincronizacion: respuesta_conest['fecha_hora'])
-                datos_conest['carreras'].each do |c|
-                  @carrera = Carrera.find_by(codigo: c['id'])
-                  c['estudiantes'].each do |e|
-                    # Obtengo el Estudiante
-                    @estudiante = Estudiante.find_or_initialize_by(cedula: e['cedula'])
-                    @estudiante.update(cedula: e['cedula'], primer_nombre: e['primer_nombre'], segundo_nombre: e['segundo_nombre'], primer_apellido: e['primer_apellido'], segundo_apellido: e['segundo_apellido'])
-
-                    # Creación de usuarios de Estudiante
-                    unless Usuario.exists?(cedula: @estudiante.cedula)
-                      rol = Rol.find_by(nombre: 'Estudiante')
-                      usuario = Usuario.create(cedula: @estudiante.cedula, clave: @estudiante.cedula, rol: rol)
-                    end
-                    
-                    e['materias'].each do |m|
-                      @materia = Materia.find_by(codigo: m['codigo'], carrera: @carrera)
-                      @oferta_periodo = @materia.ofertas_periodo.find_by(periodo_academico: @periodo_academico)
-                      @oferta_academica = @oferta_periodo.oferta_academica.find_by(oferta_periodo: @oferta_periodo, nombre_seccion: m['nombre_seccion'])
-
-                      @control_consulta = ControlConsulta.find_or_create_by(oferta_academica: @oferta_academica, estudiante: @estudiante)
-                    end
-                  end
-                end
+              when :ok
                 render json: { estatus: respuesta_conest['estatus'], mensaje: "Controles de consultas para período #{@periodo_academico.periodo} actualizados", sincronizacion: @periodo_academico.sincronizacion }, status: :ok
+              else
+                nil
               end
             else
               render json: { estatus: respuesta_conest['estatus'], mensaje: respuesta_conest['mensaje'] }, status: :bad_request
@@ -178,88 +173,6 @@ module Api
                                     :conest_token  => Token::actual
           end
           return respuesta
-        end
-
-        def sincronizar_periodo(periodo_academico, respuesta_conest, datos_conest, instrumento_id)
-          if periodo_academico.id.nil?
-            # Crear el nuevo periodo academico
-            periodo_academico.update(asignaturas_hash_sum: respuesta_conest['sha1_sum'], sincronizacion: respuesta_conest['fecha_hora'])
-            procesar_periodo(periodo_academico, datos_conest, instrumento_id)
-            render json: { estatus: respuesta_conest['estatus'], mensaje: "Período #{periodo_academico.periodo} registrado" }, status: :created
-          else
-            # Si el hash es el mismo no hay cambios en el listado
-            if periodo_academico.asignaturas_hash_sum.eql?(respuesta_conest['sha1_sum'])
-              render json: { estatus: respuesta_conest['estatus'], mensaje: "Período #{periodo_academico.periodo} sin modificaciones" }, status: :not_modified
-            else
-              # Si no es el mismo, actualizo y continuo
-              periodo_academico.update(asignaturas_hash_sum: respuesta_conest['sha1_sum'], sincronizacion: respuesta_conest['fecha_hora'])
-              procesar_periodo(periodo_academico, datos_conest, instrumento_id)
-              render json: { estatus: respuesta_conest['estatus'], mensaje: "Período #{periodo_academico.periodo} actualizado", sincronizacion: @periodo_academico.sincronizacion }, status: :ok
-            end
-          end
-        end
-
-        # Método que procesa la información recibida desde CONEST sobre el período académico registrado
-        # En 'd' se contienen los datos de la respuesta enviada por CONEST
-        def procesar_periodo(periodo_academico, datos_conest, instrumento_id = nil)
-          # Iterar sobre las organizaciones (Escuelas)
-          datos_conest['organizaciones'].each do |o|
-            # Para cada Carrera de cada Organizacion
-            o['carreras'].each do |c|
-              @carrera = Carrera.find_or_initialize_by(codigo: c['id'], organizacion_id: o['id'])
-              if @carrera.id.nil?
-                @carrera.update(nombre: c['nombre'])
-              end
-
-              # Para cada Materia de cada Carrera
-              c['materias'].each do |m|
-                @materia = Materia.find_or_initialize_by(carrera: @carrera, plan_nombre: c['plan_nombre'], codigo: m['codigo'])
-                if @materia.id.nil?
-                  @materia.update(nombre: m['nombre'], tipo_materia_id: m['tipo_materia_id'], grupo_nota_id: m['grupo_nota_id'])
-                end
-                
-                # Obtengo el Coordinador
-                @coordinador = Docente.find_or_initialize_by(cedula: m['coordinador']['cedula'])
-                @coordinador.update(m['coordinador'])
-                
-                # Creación de usuario de Coordinador
-                unless Usuario.exists?(cedula: @coordinador.cedula)
-                  rol = Rol.find_by(nombre: 'Docente')
-                  usuario = Usuario.create(cedula: @coordinador.cedula, clave: @coordinador.cedula, rol: rol)
-                end
-
-
-                # Obtengo la oferta en el periodo
-                @oferta_periodo = OfertaPeriodo.find_or_initialize_by(periodo_academico: periodo_academico, materia: @materia)
-                @oferta_periodo.update(docente_coordinador: @coordinador)
-                
-                # Para cada Seccion de cada Materia
-                m['secciones'].each do |s|
-
-                  # Obtengo el docente
-                  @docente = Docente.find_or_initialize_by(cedula: s['docente']['cedula'])
-                  @docente.update(s['docente'])
-
-                  # Creación de usuarios de Docente
-                  unless Usuario.exists?(cedula: @docente.cedula)
-                    rol = Rol.find_by(nombre: 'Docente')
-                    usuario = Usuario.create(cedula: @docente.cedula, clave: @docente.cedula, rol: rol)
-                  end
-                  
-                  # Registro la oferta academica para ese periodo
-                  @oferta_academica = OfertaAcademica.find_or_initialize_by(nombre_seccion: s['nombre'], oferta_periodo: @oferta_periodo)
-                  @oferta_academica.update(docente: @docente, promedio_general: s['promedio_general'], nro_estudiantes: s['nro_estudiantes'], nro_estudiantes_retirados: s['nro_estudiantes_retirados'], nro_estudiantes_aprobados: s['nro_estudiantes_aprobados'], nro_estudiantes_equivalencia: s['nro_estudiantes_equivalencia'], nro_estudiantes_suficiencia: s['nro_estudiantes_suficiencia'], nro_estudiantes_reparacion: s['nro_estudiantes_repararon'], nro_estudiantes_aplazados: s['nro_estudiantes_aplazados'], tipo_estatus_calificacion_id: s['tipo_status_calificacion_id'])
-
-                  @consulta = Consulta.find_or_initialize_by(oferta_academica: @oferta_academica)
-                  
-
-                  instrumento = instrumento_id.nil? ? Instrumento.all.last : Instrumento.find(instrumento_id)
-
-                  @consulta.update(instrumento: instrumento)
-                end
-              end
-            end
-          end
         end
     end
   end
--- app/models/periodo_academico.rb
+++ app/models/periodo_academico.rb
@@ -13,4 +13,121 @@ class PeriodoAcademico < ActiveRecord::Base
 				allow_nil: true,
 				allow_blank: true,
 				uniqueness: true
+
+
+    # Método que procesa la información recibida desde CONEST sobre el período académico registrado
+    # En 'd' se contienen los datos de la respuesta enviada por CONEST
+    def self.procesar_periodo(periodo_academico, datos_conest, instrumento_id = nil)
+      # Iterar sobre las organizaciones (Escuelas)
+      datos_conest['organizaciones'].each do |o|
+        # Para cada Carrera de cada Organizacion
+        o['carreras'].each do |c|
+          @carrera = Carrera.find_or_initialize_by(codigo: c['id'], organizacion_id: o['id'])
+          if @carrera.id.nil?
+            @carrera.update(nombre: c['nombre'])
+          end
+
+          # Para cada Materia de cada Carrera
+          c['materias'].each do |m|
+            @materia = Materia.find_or_initialize_by(carrera: @carrera, plan_nombre: c['plan_nombre'], codigo: m['codigo'])
+            if @materia.id.nil?
+              @materia.update(nombre: m['nombre'], tipo_materia_id: m['tipo_materia_id'], grupo_nota_id: m['grupo_nota_id'])
+            end
+            
+            # Obtengo el Coordinador
+            @coordinador = Docente.find_or_initialize_by(cedula: m['coordinador']['cedula'])
+            @coordinador.update(m['coordinador'])
+            
+            # Creación de usuario de Coordinador
+            unless Usuario.exists?(cedula: @coordinador.cedula)
+              rol = Rol.find_by(nombre: 'Docente')
+              usuario = Usuario.create(cedula: @coordinador.cedula, clave: @coordinador.cedula, rol: rol)
+            end
+
+
+            # Obtengo la oferta en el periodo
+            @oferta_periodo = OfertaPeriodo.find_or_initialize_by(periodo_academico: periodo_academico, materia: @materia)
+            @oferta_periodo.update(docente_coordinador: @coordinador)
+            
+            # Para cada Seccion de cada Materia
+            m['secciones'].each do |s|
+
+              # Obtengo el docente
+              @docente = Docente.find_or_initialize_by(cedula: s['docente']['cedula'])
+              @docente.update(s['docente'])
+
+              # Creación de usuarios de Docente
+              unless Usuario.exists?(cedula: @docente.cedula)
+                rol = Rol.find_by(nombre: 'Docente')
+                usuario = Usuario.create(cedula: @docente.cedula, clave: @docente.cedula, rol: rol)
+              end
+              
+              # Registro la oferta academica para ese periodo
+              @oferta_academica = OfertaAcademica.find_or_initialize_by(nombre_seccion: s['nombre'], oferta_periodo: @oferta_periodo)
+              @oferta_academica.update(docente: @docente, promedio_general: s['promedio_general'], nro_estudiantes: s['nro_estudiantes'], nro_estudiantes_retirados: s['nro_estudiantes_retirados'], nro_estudiantes_aprobados: s['nro_estudiantes_aprobados'], nro_estudiantes_equivalencia: s['nro_estudiantes_equivalencia'], nro_estudiantes_suficiencia: s['nro_estudiantes_suficiencia'], nro_estudiantes_reparacion: s['nro_estudiantes_repararon'], nro_estudiantes_aplazados: s['nro_estudiantes_aplazados'], tipo_estatus_calificacion_id: s['tipo_status_calificacion_id'])
+
+              @consulta = Consulta.find_or_initialize_by(oferta_academica: @oferta_academica)
+
+              instrumento = instrumento_id.nil? ? Instrumento.all.last : Instrumento.find(instrumento_id)
+
+              @consulta.update(instrumento: instrumento)
+            end
+          end
+        end
+      end
+    end
+
+	def sincronizar_periodo(respuesta_conest, datos_conest, instrumento_id)
+		if self.id.nil?
+		# Crear el nuevo periodo academico
+			self.update(asignaturas_hash_sum: respuesta_conest['sha1_sum'], sincronizacion: respuesta_conest['fecha_hora'])
+			PeriodoAcademico.procesar_periodo(self, datos_conest, instrumento_id)
+			return :created
+		else
+			# Si el hash es el mismo no hay cambios en el listado
+			if self.asignaturas_hash_sum.eql?(respuesta_conest['sha1_sum'])
+				return :not_modified
+			else
+			# Si no es el mismo, actualizo y continuo
+				self.update(asignaturas_hash_sum: respuesta_conest['sha1_sum'], sincronizacion: respuesta_conest['fecha_hora'])
+				PeriodoAcademico.procesar_periodo(self, datos_conest, instrumento_id)
+				return :ok
+			end
+		end
+    end
+
+    def sincronizar_estudiantes(respuesta_conest)
+    	# Si el hash es el mismo no hay cambios en el listado
+		if self.estudiantes_hash_sum.eql?(respuesta_conest['sha1_sum'])
+			return :not_modified
+		else
+            # Datos de la respuesta enviada por Conest
+            datos_conest = respuesta_conest['datos']
+			
+			self.update(estudiantes_hash_sum: respuesta_conest['sha1_sum'], sincronizacion: respuesta_conest['fecha_hora'])
+			datos_conest['carreras'].each do |c|
+			  carrera = Carrera.find_by(codigo: c['id'])
+			  c['estudiantes'].each do |e|
+			    # Obtengo el Estudiante
+			    estudiante = Estudiante.find_or_initialize_by(cedula: e['cedula'])
+			    estudiante.update(cedula: e['cedula'], primer_nombre: e['primer_nombre'], segundo_nombre: e['segundo_nombre'], primer_apellido: e['primer_apellido'], segundo_apellido: e['segundo_apellido'])
+
+			    # Creación de usuarios de Estudiante
+			    unless Usuario.exists?(cedula: estudiante.cedula)
+			      rol = Rol.find_by(nombre: 'Estudiante')
+			      usuario = Usuario.create(cedula: estudiante.cedula, clave: estudiante.cedula, rol: rol)
+			    end
+			    
+			    e['materias'].each do |m|
+			      materia = Materia.find_by(codigo: m['codigo'], carrera: carrera)
+			      oferta_periodo = @materia.ofertas_periodo.find_by(periodo_academico: self)
+			      oferta_academica = @oferta_periodo.oferta_academica.find_by(oferta_periodo: oferta_periodo, nombre_seccion: m['nombre_seccion'])
+
+			      control_consulta = ControlConsulta.find_or_create_by(oferta_academica: oferta_academica, estudiante: estudiante)
+			    end
+			  end
+			end
+			return :ok
+		end
+    end
 end
