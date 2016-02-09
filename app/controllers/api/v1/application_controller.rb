5d763524466b008b9f07a5f3d13600a0c5956de7
Marco Gómez <marcogonzalo@gmail.com>
2016-02-09 12:12:41 -0430

    retirados puts de verificacion

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -80,7 +80,6 @@ module Api
 
 			# CanCanCan: carga los permisos del usuario actual
 			def cargar_permisos
-				puts @current_user.inspect
 				@permisos_de_usuario = @current_user.rol.permisos.collect{|i| i.nombre}
 			end
 
--- app/controllers/api/v1/roles_controller.rb
+++ app/controllers/api/v1/roles_controller.rb
@@ -24,7 +24,6 @@ module Api
 			end
 
 			def update
-				puts params[:permisos_rol].inspect
 				@rol.permisos = []
 				@rol.asignar_permisos(params[:permisos_rol]) if params[:permisos_rol]
 				@rol.save
@@ -32,7 +31,6 @@ module Api
 		#		@permisos = Permiso.all.select{|i| ["Part"].include? i.subject_class}.compact
 				@permisos = Permiso.all.select{|p| p.nombre != 'accesoTotal' }
 				@rol_permisos = @rol.permisos.collect{|p| p.id}
-				puts @rol_permisos.inspect
 				render json: {rol: @rol, permisos: @permisos, permisos_rol: @rol_permisos }
 			end
 
